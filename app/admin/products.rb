d72c52895f8d0437211e7f68f6c62768f2637fa4
Sol Nam <cbkpwr@gmail.com>
2024-07-12 17:15:31 -0500

    Add, edit or delete product listings by way of an admin dashboard

--- app/admin/products.rb
+++ app/admin/products.rb
@@ -1,18 +1,48 @@
 ActiveAdmin.register Product do
 
-  # See permitted parameters documentation:
-  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-  #
-  # Uncomment all parameters which should be permitted for assignment
-  #
-  # permit_params :name, :description, :price, :sale_price, :new_arrival, :image_url, :stock_quantity
-  #
-  # or
-  #
-  # permit_params do
-  #   permitted = [:name, :description, :price, :sale_price, :new_arrival, :image_url, :stock_quantity]
-  #   permitted << :other if params[:action] == 'create' && current_user.admin?
-  #   permitted
-  # end
+  permit_params :name, :description, :price, :stock_quantity, :category_ids, images: []
   
+  index do
+    selectable_column
+    id_column
+    column :name
+    column :description
+    column :price
+    column :stock_quantity
+    column "Images" do |product|
+      product.images.each do |image|
+        span do
+          image_tag image.variant(resize: "100x100")
+        end
+      end
+    end
+    actions
+  end
+
+  show do
+    attributes_table do
+      row :name
+      row :description
+      row :price
+      row :stock_quantity
+      row "Images" do |product|
+        product.images.each do |image|
+          span do
+            image_tag image.variant(resize: "200x200")
+          end
+        end
+      end
+    end
+  end
+
+  form do |f|
+    f.inputs do
+      f.input :name
+      f.input :description
+      f.input :price
+      f.input :stock_quantity
+      f.input :images, as: :file, input_html: { multiple: true }, hint: f.object.images.attached? ? image_tag(url_for(f.object.images.first.variant(resize: "100x100"))) : content_tag(:span, "Upload JPG/PNG/GIF image")
+    end
+    f.actions
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,4 +7,95 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
-AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
+# Create an admin user only in development environment if it doesn't exist
+if Rails.env.development?
+  AdminUser.find_or_create_by!(email: 'admin@example.com') do |user|
+    user.password = 'password'
+    user.password_confirmation = 'password'
+  end
+end
+
+
+Category.create([
+  { name: 'Necklaces' },
+  { name: 'Bracelets' },
+  { name: 'Rings' },
+  { name: 'Nails' }  
+])
+
+Product.create([
+  {
+    name: 'Gold Letter Necklace',
+    description: 'Beautiful gold necklace with intricate design.',
+    price: 209.99,
+    stock_quantity: 15
+  },
+  {
+    name: 'Gold Letter Bracelet',
+    description: 'Beautiful gold bracelet with intricate design.',
+    price: 199.99,
+    stock_quantity: 20
+  },
+  {
+    name: 'Gold Gemini Necklace',
+    description: 'Beautiful gold gemini necklace for july-er.',
+    price: 499.99,
+    stock_quantity: 10
+  },
+  {
+    name: 'Black and Silver Nails',
+    description: '100% Hand crafted nails',
+    price: 59.99,
+    stock_quantity: 8
+  },
+  {
+    name: 'Gold Customized Necklace',
+    description: 'Design your own necklace.',
+    price: 299.99,
+    stock_quantity: 12
+  },
+  {
+    name: 'Cute Summer Necklace',
+    description: 'Casual pearl necklace for summer.',
+    price: 69.99,
+    stock_quantity: 25
+  },
+  {
+    name: 'Heart Ring',
+    description: 'Exquisite heart shape ring with a vintage setting.',
+    price: 39.99,
+    stock_quantity: 7
+  },
+  {
+    name: 'Pearl Necklace',
+    description: 'Luxurious pearl necklace fit for royalty.',
+    price: 599.99,
+    stock_quantity: 5
+  },
+  {
+    name: 'Lisa Bracelet',
+    description: 'Chic gold Bracelet for Lisa',
+    price: 159.99,
+    stock_quantity: 3
+  },
+  {
+    name: 'Star and Moon Ring',
+    description: 'Be my star, I will be your moon.',
+    price: 59.99,
+    stock_quantity: 10
+  }
+])
+
+
+ProductCategory.create([
+  { product_id: 1, category_id: 1 },  
+  { product_id: 2, category_id: 2 },  
+  { product_id: 3, category_id: 1 },  
+  { product_id: 4, category_id: 4 },  
+  { product_id: 5, category_id: 1 },  
+  { product_id: 6, category_id: 1 },  
+  { product_id: 7, category_id: 3 }, 
+  { product_id: 8, category_id: 1 },  
+  { product_id: 9, category_id: 2 },  
+  { product_id: 10, category_id: 3 }  
+])
