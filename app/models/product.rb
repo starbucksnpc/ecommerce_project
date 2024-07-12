fa6c4254520fe7640ff25d3b079cbec7da94cc20
Sol Nam <cbkpwr@gmail.com>
2024-07-12 02:13:08 -0500

    setup Active Storage

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,10 +3,13 @@ class Product < ApplicationRecord
     def self.ransackable_associations(auth_object = nil)
         ["cart_items", "categories", "order_items", "product_categories"]
       end
-      
+
     def self.ransackable_attributes(auth_object = nil)
         ["created_at", "description", "id", "id_value", "image_url", "name", "new_arrival", "price", "sale_price", "stock_quantity", "updated_at"]
       end
+
+
+    has_one_attached :image
 
 
   has_many :order_items
