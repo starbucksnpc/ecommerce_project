e9aeba92332be85a7ae5b71d26eaec9438a68dc4
Sol Nam <cbkpwr@gmail.com>
2024-07-12 19:09:18 -0500

    create routes for contact and about pages

--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,10 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
+  get '/about', to: 'pages#about'
+  get '/contact', to: 'pages#contact'
+
+
   # Defines the root path route ("/")
   root "home#index"
 end
